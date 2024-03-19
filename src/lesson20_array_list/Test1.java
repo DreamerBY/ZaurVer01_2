474ef6626f1c28bc341ba31a51bd28aebeedb2d7
WizardD <boxp@mail.ru>
2024-03-19 19:28:10 +0300

    19 03 2024 - Прошел 20 глав

---
+++ src/lesson20_array_list/Test1.java
@@ -0,0 +1,37 @@
+package lesson20_array_list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test1 {
+    public static void main(String[] args) {
+        ArrayList list = new ArrayList();
+        list.add("privet");
+        Car c = new Car();
+        list.add(c);
+        Student s = new Student();
+        list.add(s);
+        list.add(new StringBuilder("ok"));
+
+        ArrayList<String> list2 = new ArrayList<String>();
+        list2.add("poka");
+        //list2.add(s);
+        ArrayList<String> list21 = new ArrayList<>(30);
+        ArrayList<String> list5 = new ArrayList<>(list2);
+        System.out.println(list21==list5);
+
+
+
+        ArrayList<StringBuilder> list3 = new ArrayList();
+        List<StringBuilder> list31 = new ArrayList();
+        list3.add(new StringBuilder("ok"));
+
+
+    }
+}
+
+class Car {
+}
+
+class Student {
+}
---
+++ src/lesson20_array_list/Test2.java
@@ -0,0 +1,47 @@
+package lesson20_array_list;
+
+import java.util.ArrayList;
+
+public class Test2 {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("poka");
+        list.add("privet");
+        list.add("ok");
+        list.add(1, "hello");
+
+
+        for (String s : list) {
+            System.out.print(s + " ");
+        }
+
+        System.out.println();
+        ArrayList<String> list2 = new ArrayList<>();
+        list2.add("Petrov");
+        list2.add("Sidorov");
+        list.addAll(list2);
+
+
+
+       // list.addAll(list);
+      //  list.remove("poka");
+//        list.set(1,"!!!");
+        for (int i = 0; i < list2.size(); i++) {
+            System.out.print(list2.get(i) + " ");
+        }
+        //  System.out.println (list.get(2)+" ");
+        System.out.println();
+        for (String s : list) {
+            System.out.print(s + " ");
+        }
+        list2.set(1,"Ivanov");
+
+        System.out.println();
+        for (String s : list) {
+            System.out.print(s + " ");
+        }
+
+
+
+    }
+}
