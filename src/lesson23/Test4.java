9c797eff2d8fa6f3817f14a44a327aaa1391902d
WizardD <boxp@mail.ru>
2024-08-03 20:00:14 +0300

    commit ostalosj 38.39.40 - UPDATE NULL 03/08/2024

--- src/lesson23/Test4.java
+++ src/lesson23/Test4.java
@@ -1,32 +1,32 @@
 package lesson23;
 
-//public class Test4 {
-//    void abc(Animal a) {                  //OVERLOADED
-//        System.out.println("A");
-//    }
-//
-//    void abc(Mouse a) {                   //OVERLOADED
-//        System.out.println("A");
-//    }
-//
-//    public static void main(String[] args) {
-//        Test4 t = new Test4();
-//        Animal an = new Mouse();
-//
-//        t.abc(an);
-//        an.getName();
-//    }
-//}
+public class Test4 {
+    void abc(Animal1 a) {                  //OVERLOADED
+        System.out.println("A");
+    }
 
-//class Animal {
-//    void getName() {
-//        System.out.println("Animal");
-//    }
-//}
-//
-//
-//class Mouse extends Animal {
-//    void getName() {
-//        System.out.println("Mouse");
-//    }
-//}
+    void abc(Mouse1 a) {                   //OVERLOADED
+        System.out.println("M");
+    }
+
+    public static void main(String[] args) {
+        Test4 t1 = new Test4();
+        Animal1 an1 = new Mouse1();
+
+        t1.abc(an1);
+       an1.getName();
+    }
+}
+
+class Animal1 {
+    void getName() {
+        System.out.println("Animal");
+    }
+}
+
+
+class Mouse1 extends Animal1 {
+    void getName() {
+        System.out.println("Mouse");
+    }
+}
--- src/lesson23/Test5.java
+++ src/lesson23/Test5.java
@@ -1,54 +1,50 @@
 package lesson23;
 
 public class Test5 {
-    public static void main(String[] args) {
-        Employee emp = new Employee();
-        Teacher t = new Teacher();
-        //emp.sleep();
-        // t.sleep();
-        Employee e = new Teacher();
-        e.sleep();
-
-
-    }
-
+  public static void main(String[] args) {
+    Employee emp = new Employee();
+    Teacher t = new Teacher();
+    //emp.sleep();
+  //  t.sleep();
+    Employee e = new Teacher();
+    e.sleep();
+  }
 }
 
 class Employee {
+  double salary = 100;
+  String name = "Kolya";
 
-    double salary = 100;
-    String name = "Kolya";
 
+  Eda eat() {
+    System.out.println("Kushaet rabotnik");
+    Eda e = new Eda();
+    return e;
+  }
 
-    Eda eat() {
-        System.out.println("Kushaet rabotnik");
-        Eda e = new Eda();
-        return e;
-    }
-
-    static void sleep() {
-        System.out.println("Spit rabotnik");
-    }
+  static void sleep() {
+    System.out.println("Spit rabotnik");
+  }
 }
 
 class Teacher extends Employee {
 
-    @Override
-    Eda eat() {
-        System.out.println("Kushaet U4itel");
-        Fruit f = new Fruit();
-        return f;
-    }
+  @Override
+  Eda eat() {
+    System.out.println("Kushaet U4itel");
+    Fruit f = new Fruit();
+    return f;
+  }
 
-    int kolichestvoUchenikov;
+  int kolichestvoUchenikov;
 
-    void uchit() {
-        System.out.println("Uchit");
-    }
+  void uchit() {
+    System.out.println("Uchit");
+  }
 
-    static void sleep() {
-        System.out.println("Spit u4itel");
-    }
+  static void sleep() {
+    System.out.println("Spit u4itel");
+  }
 }
 //final class t{}
 //class p extends t{}
